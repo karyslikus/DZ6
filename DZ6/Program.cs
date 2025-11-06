db89347284997c0fc23443d7f3e4bfbd2fe2075b
karyslikus <morozovdaniil522@gmail.com>
2025-11-06 15:07:03 +0300

    hh
    hh

---
+++ DZ6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Reverser
+{
+    static void Main()
+    {
+        int[] a = { 10, 20, 30, 40, 50 };
+        int b = a.Length;
+        Console.Write("Исходный массив: ");
+        foreach (int c in a)
+        {
+            Console.Write(c + " ");
+        }
+        Console.WriteLine();
+        int[] d = new int[b];
+        for (int i = 0; i < b; i++)
+        {
+            int f = b - 1 - i;
+            d[i] = a[f];
+        }
+        Console.Write("Перевернутый массив: ");
+        foreach (int c in d)
+        {
+            Console.Write(c + " ");
+        }
+        Console.WriteLine();
+    }
+}
